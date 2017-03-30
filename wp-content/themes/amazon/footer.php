dde28d27d65d9f48e2f83e179d692c1e6410d946
Tier5 <work@thinkvaughn.com>
2017-03-30 20:42:52 +0530

    Chnages for the home page

---
+++ wp-content/themes/amazon/footer.php
@@ -0,0 +1,290 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+
+		</div><!-- #content -->
+
+		 <footer>
+        <div class="container">
+            <div class="col-md-3 col-sm-3">
+                <h3>Amazon Product Optimization</h3>
+                <ul>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum amet</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    
+                </ul>
+            </div> 
+            <div class="col-md-3 col-sm-3">
+                <h3>Let Us Help You</h3>
+                <ul>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum amet</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum amet</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum amet</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                </ul>
+            </div> 
+            <div class="col-md-3 col-sm-3">
+                <h3>FAQ</h3>
+                <ul>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum amet</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum amet</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum amet</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                </ul>
+            </div> 
+            <div class="col-md-3 col-sm-3">
+                <h3>Get to Know Us</h3>
+                <ul>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum amet</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum amet</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                    <li><a href="#">Lorem ipsum amet</a></li>
+                    <li><a href="#">Lorem ipsum sit</a></li>
+                </ul>
+            </div>  
+        </div> 
+        <div class="container copyrights">
+          <div class="row">  
+            <div class="col-md-12 col-sm-12">
+                <hr>
+             Copyright © 2017 Amazon Optimization, All Rights Reserved.
+            
+            
+            </div>
+          </div>    
+        </div>
+
+        <div class="chat-icon">
+            <a href="#" id="addClass">
+              <img src="images/chat-icon.png" alt="img">
+            </a>
+        </div>
+
+    </footer>
+<div class="popup-box chat-popup" id="qnimate">
+
+        <div id="div1">
+            <div class="popup-head">
+                <div class="popup-head-arrow">
+                  <a id="btn1" href="#">
+                    <i class="fa fa-angle-left" aria-hidden="true"></i>
+                  </a>
+
+                </div>
+                <div class="popup-head-left">
+        
+                  <!-- <div class="chat-image">
+                  <img src="images/profile_img.png" alt="img"> 
+                  </div> -->
+                  <div class="chat-person">
+                  <h3>  
+                  Conversations
+                  </h3>
+                  </div>
+                  <div class="clearfix"></div>
+                </div> 
+            </div> 
+            <div class="popup-messages"> 
+                <div class="chat-head">
+                    <div class="chat-pic">
+                <img src="images/profile_img.png" alt="img">
+            </div>
+            <div class="chat-txt grey">
+             Hi there! <br>
+              Let us know if you have any questions, we are here to help. 
+            </div>
+            <div class="clearfix"></div>
+                </div> 
+
+
+                <button class="btn conversation"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> New Conversation</button>
+
+            </div>  
+
+        </div>
+
+
+          <div class="popup-head">
+        <div class="popup-head-arrow">
+        <a href="#" id="btn">
+          <i class="fa fa-angle-left" aria-hidden="true"></i>
+        </a>  
+        </div> 
+
+        <div class="popup-head-left">
+        
+        <div class="chat-image">
+        <img src="images/profile_img.png" alt="img"> 
+        </div>
+        <div class="chat-person">
+        Gurdeep Osahan <br> <small>Active</small>
+        </div>
+        <div class="clearfix"></div>
+        </div>
+        
+        <button data-widget="remove" id="removeClass" class="chat-header-button pull-right" type="button"><i class="glyphicon glyphicon-off"></i></button>
+            <!-- <div class="popup-head-right pull-right">
+            <div class="btn-group">
+                      <button class="chat-header-button" data-toggle="dropdown" type="button" aria-expanded="false">
+                     <i class="glyphicon glyphicon-cog"></i> </button>
+                    <ul role="menu" class="dropdown-menu pull-right">
+                    <li><a href="#">Media</a></li>
+                    <li><a href="#">Block</a></li>
+                    <li><a href="#">Clear Chat</a></li>
+                    <li><a href="#">Email Chat</a></li>
+                    </ul>
+            </div>
+            
+            <button data-widget="remove" id="removeClass" class="chat-header-button pull-right" type="button"><i class="glyphicon glyphicon-off"></i></button>
+                      </div> -->
+        </div>
+
+      <div class="popup-messages">
+          
+      <div class="chat-head">
+          <div class="chat-pic">
+                <img src="images/profile_img.png" alt="img">
+          </div>
+          <div class="chat-txt grey">
+              Hi there! <br>
+              Let us know if you have any questions, we are here to help.
+          </div>
+          <div class="clearfix"></div>
+      </div>
+      <div class="chat-head">
+          <div class="chat-txt yellow">
+              Tell me the full form of Google
+          </div>
+          <div class="clearfix"></div>
+
+      </div>
+      <div class="chat-head">
+          <div class="chat-pic">
+                <img src="images/profile_img.png" alt="img">
+          </div>
+          <div class="chat-txt grey">
+             What the...
+          </div>
+          <div class="clearfix"></div>
+      </div>
+      <div class="chat-head">
+          <div class="chat-txt yellow">
+              Tell me the full form of Google
+          </div>
+          <div class="clearfix"></div>
+
+      </div>
+      <div class="chat-head">
+          <div class="chat-pic">
+                <img src="images/profile_img.png" alt="img">
+          </div>
+          <div class="chat-txt grey">
+             What the...
+          </div>
+          <div class="clearfix"></div>
+      </div>
+      
+      
+      
+      </div>
+      <div class="popup-messages-footer">
+      <textarea id="status_message" placeholder="Type a message..." rows="10" cols="40" name="message"></textarea>
+      <div class="btn-footer">
+      <button class="bg_none"><i class="glyphicon glyphicon-film"></i> </button>
+      <button class="bg_none"><i class="glyphicon glyphicon-camera"></i> </button>
+            <button class="bg_none"><i class="glyphicon glyphicon-paperclip"></i> </button>
+      <button class="bg_none pull-right"><i class="glyphicon glyphicon-thumbs-up"></i> </button>
+      </div>
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+     
+
+<script type="text/javascript">
+    $(function(){
+$("#addClass").click(function () {
+          $('#qnimate').addClass('popup-box-on');
+            });
+          
+            $("#removeClass").click(function () {
+          $('#qnimate').removeClass('popup-box-on');
+            });
+  })
+</script>
+
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
+
+  <!-- Search section jquery -->
+
+  <script src="<?php bloginfo('template_directory'); ?>/js/jquery.nice-select.min.js"></script>
+  <script src="<?php bloginfo('template_directory'); ?>/js/fastclick.js"></script>
+  <script src="<?php bloginfo('template_directory'); ?>/js/prism.js"></script>
+
+  <script src="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.9/jquery-ui.js" type="text/javascript"></script>
+    <link href="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.9/themes/blitzer/jquery-ui.css"
+        rel="stylesheet" type="text/css" />
+
+  <script>
+    $(document).ready(function() {
+      $('select:not(.ignore)').niceSelect();      
+      FastClick.attach(document.body);
+    });    
+  </script>
+  
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-64633646-1', 'auto');
+    ga('send', 'pageview');
+  </script>
+
+  <!-- Search section jquery -->
+
+<script type="text/javascript">
+  
+  $('#btn').click(function() {   
+    $('#div1').animate({width:'toggle'},350);
+});
+  $('#btn1').click(function() {   
+    $('#div1').animate({width:'toggle'},350);
+});
+
+</script>
+<?php wp_footer(); ?>
+
+</body>
+</html>
